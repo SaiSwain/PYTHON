f679306335521770bd5eb6ce0090b849a6940cd6
SaiSwain <132897931+SaiSwain@users.noreply.github.com>
2023-05-19 18:51:24 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,18 @@
+# sai=[2,4,6,8,5,10,12,14,18,7]
+# for i in sai:
+#     if (i%2==0): 
+#          print(i)
+#     else:
+#         continue
+
+# chiku=input("Enter your name")
+# print(chiku)
+
+# hii=input("Enter number")
+# print(hii)
+
+
+# sai=int(input("enter your number"))
+# print(sai) 
+  
+      
